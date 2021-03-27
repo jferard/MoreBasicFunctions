ff05435ae1beb8e65caf615b0ec16ebf92edb297
Julien Férard <julien_ferard@yahoo.fr>
2021-03-27 15:58:22 +0100

    Add skeleton for Regexes, Collections, Paths, Dates.

---
+++ src/main/java/com/github/jferard/mbfs/Dates.java
@@ -0,0 +1,56 @@
+/*
+ * MoreBasicFunctions - Java - UNO component that provides more basic functions.
+ * Copyright (C) 2021 Julien Férard.
+ *
+ * MoreBasicFunctions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MoreBasicFunctions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.jferard.mbfs;
+
+import com.sun.star.lang.XServiceInfo;
+import com.sun.star.lib.uno.helper.WeakBase;
+import com.sun.star.uno.XComponentContext;
+
+public class Dates extends WeakBase
+        implements XServiceInfo, XDates {
+    public static final String implementationName = Dates.class.getName();
+
+    public static final String[] serviceNames = {"com.github.jferard.mbfs.Dates"};
+
+    private final XComponentContext xContext;
+
+    public Dates(XComponentContext context) {
+        xContext = context;
+    }
+
+    @Override
+    public String getImplementationName() {
+        return implementationName;
+    }
+
+    @Override
+    public String[] getSupportedServiceNames() {
+        return serviceNames;
+    }
+
+    @Override
+    public boolean supportsService(String serviceName) {
+        for (String name : serviceNames) {
+            if (serviceName.equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/github/jferard/mbfs/XDates.java
@@ -0,0 +1,11 @@
+// 
+// Decompiled by Procyon v0.5.32
+// 
+
+package com.github.jferard.mbfs;
+
+import com.sun.star.uno.XInterface;
+
+public interface XDates extends XInterface
+{
+}
